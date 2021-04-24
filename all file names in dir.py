42a4fcc276738370da4afa84bb4d822259d2c836
Nandu Goud <naga12devops@gmail.com>
2021-04-24 12:54:12 +0530

    change

--- all file names in dir.py
+++ all file names in dir.py
@@ -1,5 +1,6 @@
 from os import listdir
 from os.path import isfile, join
+#mypath is to spacify on local 
 mypath = 'D:\AZ-104'
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 for file in onlyfiles:
